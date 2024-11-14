d30d39072f9c2ce6ff3d256a8a9586e72008a6b8
Raylan Araujo <raylanra@gmail.com>
2024-11-14 11:38:19 -0300

    Criar as variáveis para recuperar os dados

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function comprar() {
+    let tipo = document.getElementById('tipo-ingresso').value;
+    let quantidade = document.getElementById('qtd').value;
+
+    let pistaDisponível = parseInt(document.getElementById('qtd-pista').textContent);
+    let superiorDisponivel = parseInt(document.getElementById('qtd-superior').textContent);
+    let inferiorDisponivel = parseInt(document.getElementById('qtd-inferior').textContent);
+    
+    
+}
