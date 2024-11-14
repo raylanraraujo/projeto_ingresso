00f0c19edbda3d4b7a5cdfc575a1bb10ac3b5ea5
Raylan Araujo <raylanra@gmail.com>
2024-11-14 12:36:56 -0300

    Criar validação para a compra nao exceder a quatidade disponível

--- js/app.js
+++ js/app.js
@@ -6,18 +6,32 @@ function comprar() {
     let tipo = document.getElementById('tipo-ingresso').value;
     let quantidade = document.getElementById('qtd').value;
 
+
     if (tipo == 'inferior' && inferiorDisponivel >= 1){
-        let resultado = inferiorDisponivel - quantidade;
-        inferiorDisponivel = resultado;
-        document.getElementById('qtd-inferior').innerHTML = inferiorDisponivel;
+        if ( quantidade > inferiorDisponivel){
+            alert('Quantidade excede à quantidade disponível');
+        } else {
+            let resultado = inferiorDisponivel - quantidade;
+            inferiorDisponivel = resultado;
+            document.getElementById('qtd-inferior').innerHTML = inferiorDisponivel;
+        }
+
     } else if (tipo == 'superior' && superiorDisponivel >= 1){
-        let resultado = superiorDisponivel - quantidade;
-        superiorDisponivel = resultado;
-        document.getElementById('qtd-superior').innerHTML = superiorDisponivel;
+        if (quantidade > superiorDisponivel){
+            alert('Quantidade excede à quantidade disponível');
+        } else {
+            let resultado = superiorDisponivel - quantidade;
+            superiorDisponivel = resultado;
+            document.getElementById('qtd-superior').innerHTML = superiorDisponivel;  
+        }
     } else if (tipo == 'pista' && pistaDisponivel >= 1){
-        let resultado = pistaDisponivel - quantidade;
-        pistaDisponivel = resultado;
-        document.getElementById('qtd-pista').innerHTML = pistaDisponivel;
+        if (quantidade > pistaDisponivel){
+            alert('Quantidade excede à quantidade disponível');
+        } else {
+            let resultado = pistaDisponivel - quantidade;
+            pistaDisponivel = resultado;
+            document.getElementById('qtd-pista').innerHTML = pistaDisponivel;
+        }
     } else {
         alert('Não há ingressos desse tipo disponíveis');
     }
