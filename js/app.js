9687bfefc99cac08c9b030cfc378951141406bd2
Raylan Araujo <raylanra@gmail.com>
2024-11-14 12:21:33 -0300

    Criar as condicionais

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
+let pistaDisponivel = parseInt(document.getElementById('qtd-pista').textContent);
+let superiorDisponivel = parseInt(document.getElementById('qtd-superior').textContent);
+let inferiorDisponivel = parseInt(document.getElementById('qtd-inferior').textContent);
+
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso').value;
     let quantidade = document.getElementById('qtd').value;
 
-    let pistaDisponível = parseInt(document.getElementById('qtd-pista').textContent);
-    let superiorDisponivel = parseInt(document.getElementById('qtd-superior').textContent);
-    let inferiorDisponivel = parseInt(document.getElementById('qtd-inferior').textContent);
-    
-    
+    if (tipo == 'inferior' && inferiorDisponivel >= 1){
+        let resultado = inferiorDisponivel - quantidade;
+        inferiorDisponivel = resultado;
+        document.getElementById('qtd-inferior').innerHTML = inferiorDisponivel;
+    } else if (tipo == 'superior' && superiorDisponivel >= 1){
+        let resultado = superiorDisponivel - quantidade;
+        superiorDisponivel = resultado;
+        document.getElementById('qtd-superior').innerHTML = superiorDisponivel;
+    } else if (tipo == 'pista' && pistaDisponivel >= 1){
+        let resultado = pistaDisponivel - quantidade;
+        pistaDisponivel = resultado;
+        document.getElementById('qtd-pista').innerHTML = pistaDisponivel;
+    } else {
+        alert('Não há ingressos desse tipo disponíveis');
+    }
 }
